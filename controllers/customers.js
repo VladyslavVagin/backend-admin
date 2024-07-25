770346f1b5559fe87f3052abff82da7f5455a97e
VladyslavVagin <vladtwince@gmail.com>
2024-07-25 21:20:17 +0200

    change getAll controller
    for receive query and make possible search by query from frontend

--- controllers/customers.js
+++ controllers/customers.js
@@ -2,21 +2,29 @@ const { Customer } = require("../models/customers.js");
 const { ctrlWrapper } = require("../helpers");
 
 const getAllCustomers = async (req, res) => {
-    const customers = await Customer.find();
-    res.json(customers);
+  const { query } = req.query;
+  let customers;
+  if (query) {
+    customers = await Customer.find({
+      name: { $regex: query, $options: "i" },
+    });
+  } else {
+    customers = await Customer.find();
+  }
+  res.json(customers);
 };
 
 const getCustomer = async (req, res) => {
-    const { _id } = req.params;
-    const customer = await Customer.findOne({ _id: _id });
-    if (customer) {
-        res.json(customer);
-    } else {
-        res.status(404).json({ message: 'Customer not found' });
-    }
-}
+  const { _id } = req.params;
+  const customer = await Customer.findOne({ _id: _id });
+  if (customer) {
+    res.json(customer);
+  } else {
+    res.status(404).json({ message: "Customer not found" });
+  }
+};
 
 module.exports = {
-    getAllCustomers: ctrlWrapper(getAllCustomers),
-    getCustomer: ctrlWrapper(getCustomer)
-  };
+  getAllCustomers: ctrlWrapper(getAllCustomers),
+  getCustomer: ctrlWrapper(getCustomer),
+};
